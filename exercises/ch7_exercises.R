27cbb0b2efea03c9c444c21dcd2766820c7d5727
Mike Todisco <mtodisco@splashthat.com>
2018-08-15 18:09:49 -0400

    continuing chapter 7

--- exercises/ch7_exercises.R
+++ exercises/ch7_exercises.R
@@ -1,11 +1,8 @@
 ##### 1 Run Value of Individual Pitches
 #a) 
-pbp2011 <- read.csv('DataFiles/retrosheets/events/all2011.csv')
+pbp2011 <- read.csv('DataFiles/retrosheets/events/all2011.csv', stringsAsFactors = FALSE)
 header <- read.csv('DataFiles/retrosheets/events/fields.csv')
 names(pbp2011) <- names(header)
-
-pbp2011rc <- read.csv('https://raw.githubusercontent.com/maxtoki/baseball_R/master/data/pbp11rc.csv')
-head(pbp2011rc)
 
 ##### 2 Length of Plate Appearances
 #a) 
@@ -19,13 +16,17 @@ pbp2011 %>%
   group_by(lineup_position) %>%
   summarise(avg_ab_length = mean(ab_length, na.rm = TRUE))
 
-head(pbp2011)
 #b)
 pbp2011 %>%
   filter(lineup_postition == 8) %>%
   group_by()
 
-##### 3 Pickoff Attempts
-pbp2011 <- mutate(pbp2011, pick_off_attempts_from_2nd = sum(grepl(2, pbp2011$pitch_sequence)))
-head(pbp2011)
+##### 3 Pickoff Attempts to 2nd Base
 
+countCharOccurrences <- function(char, s) {
+  s2 <- gsub(char,"",s)
+  return (nchar(s) - nchar(s2))
+}
+
+pbp2011 <- mutate(pbp2011, second_base_pickoff_count = countCharOccurrences("2", pitch_sequence))
+head
