df4c0f877a3d970573c6aa6b27f2259f1058f360
Mike Todisco <mtodisco@splashthat.com>
2018-08-16 16:29:11 -0400

    still working through ch7

--- exercises/ch7_exercises.R
+++ exercises/ch7_exercises.R
@@ -9,7 +9,6 @@ names(pbp2011) <- names(header)
 library(dplyr)
 #removing all non-pitches from the pitch sequence using regular expression
 pbp2011$pseq <- gsub("[.>123N+*]", "", pbp2011$pitch_sequence)
-#pbp2011$ab_length <- apply(pbp2011[, c('pseq')], 2, nchar)
 pbp2011 <- mutate(pbp2011, ab_length = nchar(pseq))
 
 pbp2011 %>%
@@ -29,4 +28,15 @@ countCharOccurrences <- function(char, s) {
 }
 
 pbp2011 <- mutate(pbp2011, second_base_pickoff_count = countCharOccurrences("2", pitch_sequence))
-head
+#2nd base unoccupied
+
+##### 4 Umpire's Strike Zone
+load("DataFiles/balls_strikes_count.RData")
+umpiresFF_R <- subset(umpires, (pitch_type == 'FF') & (batter_hand == 'R'))
+umpiresFF_R.loess <- loess(called_strike ~ px + pz, data = umpiresFF_R[sample(1:nrow(umpiresFF_R), 300),], control = loess.control(surface = 'direct'))
+umpFF_R_contour <- contourLines(x=seq(-2, 2, 0.1),
+                                y=seq(0, 6, 0.1),
+                                z=predict(umpiresFF_R.loess),
+                                levels=c(.5))
+
+predict(umpiresFF_R.loess)
